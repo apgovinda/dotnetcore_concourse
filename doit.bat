3d6f242cc2a1692198239d7eb7683050357a8ee1
GOVINDHARAJU PALANI <apgovinda@gmail.com>
2019-12-03 22:21:24 +0530

    03-12-2019-22:21:24.07

--- doit.bat
+++ doit.bat
@@ -1,3 +1,9 @@
+REM ./fly -t main set-pipeline -p dotnet-core-helloWorld -c concourse/pipeline.yml -l concourse/credentials-local.yml
+REM  ./fly -t main unpause-pipeline -p dotnet-core-helloWorld
+REM ./fly -t main destroy-pipeline -p dotnet-core-helloWorld
+
 git add .
 git commit -m"%DATE%-%TIME%"
 git push -u origin master
+
+
